a86ddb6183a914c29c4bd3dd90d10c7f9305cb45
TheCodeOff <PanicStormRegister@gmail.com>
2013-11-02 12:03:49 +0200

    Gallery now functional. Uses a directory to fetch images instead of database.

--- gallery.php
+++ gallery.php
@@ -4,23 +4,68 @@ $title = ".gallery";
 include("include/header.inc");
 include("include/sidebar.inc");
 
-$image_list = array(
-    'img/ape.jpg',
-    'img/avatar.png',
-    'img/brains.jpg',
-    'img/shirt.jpg'
-);
+$imgdir = "img";
+// open this directory
+$myDirectory = opendir($imgdir);
+// get each image
+while ($entryName = readdir($myDirectory)) {
+    $ext = pathinfo($entryName, PATHINFO_EXTENSION);
+    if ($ext == 'jpg' || $ext == 'png' || $ext == 'gif') {
+        $image_list[] = $entryName;
+        echo $entryName;
+    }
+}
+// close directory
+closedir($myDirectory);
+//	count elements in array
+$indexCount = count($image_list);
+// sort 'em
+sort($image_list);
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $target_path = "img/";
+    $target_path = $target_path . basename($_FILES['image']['name']);
+    $target_ext = pathinfo($entryName, PATHINFO_EXTENSION);
+    if (!isset($_FILES['image']) || $_FILES['image']['name'] == '') {
+        informUser("Please specify a image!");
+    } else if ($target_ext != 'jpg' && $target_ext != 'png' && $target_ext != 'gif') {
+        informUser("You can only upload jpeg and png images!");
+    } else {
+        $result = move_uploaded_file($_FILES['image']['tmp_name'], $target_path); //Upload file to target path
+        if ($result) {
+            informUser("Successfully added image to gallery!");
+        } else {
+            informUser("There was an unprecedented error uploading file!");
+        }
+    }
+    // Redirect
+    header("refresh:0;gallery.php");
+    die();
+}
 
 ?>
     <table class="content" cellpadding="0" cellspacing="0">
         <tr>
-            <td class="content_header" colspan="3">Gallery</td>
+            <td class="content_header" colspan="3">
+                Gallery
+                <?php if (isset($_GET['imageid'])) {
+                    echo '<a href="gallery.php">Back to Gallery</a>';
+                }?>
+            </td>
         </tr>
         <tr>
             <td id="gallery">
                 <?php
-                foreach ($image_list as $image) {
-                    echo '<a href="' . $image . '"><img src="' . $image . '" alt="' . $image . '"></a>';
+                if (isset($_GET['imageid']) && $_GET['imageid'] < count($image_list)) {
+                    $image_entry = $imgdir . "/" . $image_list[$_GET['imageid']];
+                    echo '<img class="standalone" src="' . $image_entry . '" alt="' . $image_entry . '">';
+                } else {
+                    $index = 0;
+                    foreach ($image_list as $image) {
+                        $image_entry = $imgdir . "/" . $image;
+                        echo '<a href="gallery.php?imageid=' . $index . '"><img src="' . $image_entry . '" alt="' . $image_entry . '"></a>';
+                        $index++;
+                    }
                 }
                 ?>
             </td>

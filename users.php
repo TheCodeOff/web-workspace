73c5095c122cb6f84a6a836a3df078babaa3a5a7
TheCodeOff <PanicStormRegister@gmail.com>
2013-11-02 16:42:30 +0200

    Fixing the users part.

---
+++ users.php
@@ -0,0 +1,28 @@
+<?php
+include("include/common.inc");
+$title = ".users";
+include("include/header.inc");
+include("include/sidebar.inc");
+
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // ADD OR DELETE USER
+}
+
+if ($_SERVER["REQUEST_METHOD"] == "GET") {
+    // DISPLAY USERS
+}
+?>
+
+<table class="content">
+    <tr>
+        <td class="content_header" colspan="3">
+            Users
+        </td>
+    </tr>
+    <tr>
+        <td>
+            FEF
+        </td>
+    </tr>
+</table>
